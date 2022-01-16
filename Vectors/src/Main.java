c4c65f6667814007c23c825d4957dc66cd2ce146
TimMukhin <muhintimofey@mail.ru>
2022-01-16 17:41:21 +0700

    solves a task

--- Vectors/src/Main.java
+++ Vectors/src/Main.java
@@ -1,5 +1,45 @@
 public class Main {
     public static void main(String[] args) {
 
+        double[] array = {1, 5, 2, 4, 7, 9};
+        double[] array2 = {1, 5, 2, 4, 7, 9};
+        double[] array3 = {7, 2, 3, 5, 8, 6};
+        int n = array.length;
+
+        Vector vector = new Vector(array);
+        Vector vector1 = new Vector(vector);
+        Vector vector2 = new Vector(n);
+        Vector vector3 = new Vector(n, array);
+
+        Vector vector4 = new Vector(array2);
+        Vector vector5 = new Vector(array3);
+
+        System.out.println("vector = " + vector);
+        System.out.println("vector1 = " + vector1);
+        System.out.println("vector2 = " + vector2);
+        System.out.println("vector3 = " + vector3);
+        System.out.println("vector5 = " + vector5);
+
+        System.out.println("Vector size = " + vector.getSize());
+        System.out.println("Adding another vector to a vector = " + vector.add(vector4));
+        System.out.println("Subtracting another vector from a vector(vector3-vector5) " + vector3.div(vector5));
+
+        double skalar=3;
+        System.out.println("Multiplying a vector by a scalar = "+vector3.multScalar(skalar));
+        System.out.println("Vector reverse = "+vector1.getRotation());
+        System.out.println("Get lenght vector = "+vector.getLenght());
+
+        System.out.println(vector);
+        System.out.println(vector.getElement(2));
+        System.out.println(vector.setElement(2,10));
+        System.out.println(vector);
+
+        System.out.println(vector+"  "+vector1+"  "+vector2+"  "+vector3+"  "+vector4+"  "+vector5);
+        System.out.println("Equals = " + vector.equals(vector1));
+        System.out.println("Static additional of vectors = "+Vector.add(vector3,vector4));
+        System.out.println("Static substracting of vectors = "+Vector.div(vector,vector5));
+        System.out.println("Scalar multiplied of vectors = "+Vector.mult(vector3,vector5));
+
+
     }
 }
--- Vectors/src/Vector.java
+++ Vectors/src/Vector.java
@@ -1,2 +1,150 @@
+import java.util.Arrays;
+
 public class Vector {
+    private double[] vector;
+
+    // конструктор размерностью n
+    public Vector(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Поменяйте размерность вектора!");
+        }
+        this.vector = new double[n];
+    }
+
+    // конструктор с массивом
+    public Vector(double[] vector) {
+        this.vector = vector;
+    }
+
+    // конструктор копирования
+    public Vector(Vector anotherVector) {
+        this.vector = anotherVector.vector;
+    }
+
+    //заполнение вектора из массива размерностью n
+    public Vector(int n, double[] vector) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Поменяйте размерность вектора!");
+        } else if (vector.length > n) {
+            this.vector = Arrays.copyOf(vector, n);
+        } else {
+            this.vector = Arrays.copyOf(vector, vector.length);
+        }
+    }
+
+    // получение размерности вектора
+    public int getSize() {
+        return vector.length;
+    }
+
+    // реализация метода toString
+    @Override
+    public String toString() {
+        return Arrays.toString(vector);
+    }
+
+    //сложение векторов
+    public Vector add(Vector anotherVector) {
+        int vectorLenght = Math.max(vector.length, anotherVector.vector.length);
+        for (int i = 0; i < vectorLenght; i++) {
+            vector[i] += anotherVector.vector[i];
+        }
+        return this;
+    }
+
+    //вычитание векторов
+    public Vector div(Vector anotherVector) {
+        int vectorLenght = Math.max(vector.length, anotherVector.vector.length);
+        for (int i = 0; i < vectorLenght; i++) {
+            vector[i] = vector[i]- anotherVector.vector[i];
+        }
+        return this;
+    }
+
+    // умножение вектора на скаляр
+    public Vector multScalar(double scalar) {
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = vector[i] * scalar;
+        }
+        return this;
+    }
+
+    //разврот вектора
+    public Vector getRotation() {
+        return multScalar(-1);
+    }
+
+    // длина вектора
+    public double getLenght() {
+        double res = 0;
+        for (double v : vector) {
+            res += Math.pow(v, 2);
+        }
+        return Math.sqrt(res);
+    }
+
+    //получение компонента по индексу
+    public double getElement(int index) {
+        return vector[index];
+    }
+
+    //установка компонента по индексу
+    public double setElement(int index, double element) {
+        return vector[index] = element;
+    }
+
+    //переопределение метода equals
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        } else if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        } else {
+            Vector p = (Vector) obj;
+            if (p.vector.length != vector.length) {
+                return false;
+            }
+            for (int i = 0; i < vector.length; i++) {
+                if (p.vector[i] != vector[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    //переопределения метода hashCode
+    @Override
+    public int hashCode() {
+        final int prime = 37;
+        int hash = 1;
+        hash = prime * hash + Arrays.hashCode(vector);
+        return hash;
+    }
+
+    //скалярное произведение векторов
+    public double mult(Vector anotherVector) {
+        double s = 0;
+        int vectorLenght=Math.max(vector.length, anotherVector.vector.length);
+        for (int i = 0; i < vectorLenght; i++) {
+            s += vector[i] * anotherVector.vector[i];
+        }
+        return s;
+    }
+
+    public static double mult(Vector a, Vector b) {
+        return a.mult(b);
+    }
+
+    //статический метод сложения векторов
+    public static Vector add(Vector vector1, Vector anotherVector){
+
+        return vector1.add(anotherVector);
+    }
+    //статический метод вычетания векторов
+    public static Vector div(Vector vector1, Vector anotherVector){
+        return vector1.div(anotherVector);
+    }
+
 }
